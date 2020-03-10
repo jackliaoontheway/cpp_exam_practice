1283ec2617d3e757468b142df15fa24b75154f9f
Jack <ldhontheway@live.com>
2020-03-10 22:37:47 +0800

    1-6 implementation

--- 1-6.cpp
+++ 1-6.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+void swap(int,int);
+void swapRel(int&,int&);
+
+void swap(int a,int b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void swapRel(int &a,int &b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+
 int main() {
 	
+	int a = 10,b = 20;
+	swap(a,b);
+	cout << a << b;
 	
-	
+	int c = 10,d = 20;
+	swapRel(c,d);
+	cout << c << d;
 }
